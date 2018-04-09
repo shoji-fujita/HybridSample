5ab8d31447f66141892a88a4b58bf2a3890cdca2
shoji-fujita <shoji.fujita.0819@gmail.com>
2018-04-10 00:25:25 +0900

    javascriptからネイティブへの通知をAndroid側も考慮して修正

--- HybridSample/WebPage/js.js
+++ HybridSample/WebPage/js.js
@@ -1,11 +1,24 @@
 
-// ネイティブ → WebView
+// ネイティブ側に通知
+function notificationToNative(val) {
+  const isIOS = /[ \(]iP/.test(navigator.userAgent)
+  if (isIOS) {
+    // iOS側に通知
+    webkit.messageHandlers.callbackHandler.postMessage(val);
+  } else {
+    // Android側に通知
+    alert(val);
+  };
+}
+
+// ネイティブ側からjavascriptを実行
 function changeDisplay(val) {
   var label = document.getElementById("label");
   label.textContent = val;
 }
 
+// WebViewのメニューボタンタップ時
 function tappedMenuButton() {
-  // WebView → ネイティブ
-  webkit.messageHandlers.callbackHandler.postMessage("tappedMenuButton()");
+  // ネイティブ側に通知
+  notificationToNative("tappedMenuButton()");
 }
